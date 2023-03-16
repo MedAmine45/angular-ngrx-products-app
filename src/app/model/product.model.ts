3f5df068afd08a57611f12bd3512125f3a3934e0
Med amine Ben Noureddine <mohamed.bennoureddine@outlook.com>
2023-03-16 15:20:27 +0100

    base

---
+++ src/app/model/product.model.ts
@@ -0,0 +1,9 @@
+export class Product {
+    id : number =0;
+    name: string  = '';
+    price: number = 0; 
+    quantity : number = 0;
+    selected :boolean = false;
+    available : boolean = false ;  
+
+}
